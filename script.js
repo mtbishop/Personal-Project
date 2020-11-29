897cdd33d7c5526ccd02624740b66631227474a8
mtbishop <bishopmatthew76@gmail.com>
2020-11-28 18:50:58 -0600

    first commit, plenty of html css and javascript

---
+++ script.js
@@ -0,0 +1,10 @@
+function myFunction() {
+  var person = prompt('Please enter your name', 'Matthew');
+  if (person != null) {
+    document.getElementById('demo').innerHTML =
+      'Hello ' +
+      person +
+      '! How are you today? Are you ready to look at some juicy bois?';
+  }
+}
+myFunction();
